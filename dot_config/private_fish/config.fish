d9e05194858e03ad55a0cfa6793e1fa4d3ca1a09
Jeffrey Kim <kimjeffrey96@gmail>
2025-02-21 00:40:20 -0800

    Add fish alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias v="nvim"
+    alias vi="nvim"
+    alias cfg="cd ~/.config"
+    alias lg="lazygit"
+    alias ld="lazydocker"
 end
 
 # pnpm
 set -gx PNPM_HOME "/home/jeff/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
